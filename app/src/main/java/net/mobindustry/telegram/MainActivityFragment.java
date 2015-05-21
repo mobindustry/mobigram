cb182212db033fc21b50089831894dae9a51b4e3
alex.tsymbal <alex.tsymbal@mobindustry.net>
2015-05-21 12:02:10 +0300

    add telegram logo

--- app/src/main/java/net/mobindustry/telegram/MainActivityFragment.java
+++ app/src/main/java/net/mobindustry/telegram/MainActivityFragment.java
@@ -6,10 +6,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-
-/**
- * A placeholder fragment containing a simple view.
- */
 public class MainActivityFragment extends Fragment {
 
     public MainActivityFragment() {
