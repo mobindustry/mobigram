810bc4222c87618260a3a3bbc2204bc6e44cbd6c
Oleg Vovk <oleg.vovk@mobindustry.net>
2015-05-26 13:07:34 +0300

    Add RegistrationMainFragment

--- app/src/main/java/net/mobindustry/telegram/ui/activitys/RegistrationActivity.java
+++ app/src/main/java/net/mobindustry/telegram/ui/activitys/RegistrationActivity.java
@@ -21,5 +21,6 @@ public class RegistrationActivity extends Activity{
 
 
 
+
     }
 }
---
+++ app/src/main/java/net/mobindustry/telegram/ui/fragments/RegistrationMainFragment.java
@@ -0,0 +1,22 @@
+package net.mobindustry.telegram.ui.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import net.mobindustry.telegram.R;
+
+/**
+ * Created by alytar on 26.05.15.
+ */
+public class RegistrationMainFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        inflater.inflate()
+    }
+}
