77ea16d3b0602c3ea875f539e9880ad6349fffdf
Oleg Vovk <oleg.vovk@mobindustry.net>
2015-05-26 12:36:28 +0300

    Add CardView

--- app/src/main/java/net/mobindustry/telegram/RegistrationActivity.java
+++ app/src/main/java/net/mobindustry/telegram/RegistrationActivity.java
@@ -1,12 +1,23 @@
 package net.mobindustry.telegram;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 
-public class RegistratonActivity extends Activity{
+public class RegistrationActivity extends Activity{
+
+    private Fragment registrationUserPhone;
+    private Fragment chooseCountry;
+    private FragmentTransaction fragmentTransaction;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registration_activity);
+
+
+
+
     }
 }
