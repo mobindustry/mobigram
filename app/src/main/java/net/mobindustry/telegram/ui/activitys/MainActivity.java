7c904a0d0dcf72b5e76b05b42dfb5058cf1508f1
alex.tsymbal <alex.tsymbal@mobindustry.net>
2015-05-26 12:35:36 +0300

    add structure

---
+++ app/src/main/java/net/mobindustry/telegram/ui/activitys/MainActivity.java
@@ -0,0 +1,19 @@
+package net.mobindustry.telegram.ui.activitys;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import net.mobindustry.telegram.R;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+}
