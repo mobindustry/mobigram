b7a07a7f72bd75f799a074b618b87e427dea4af3
Oleg Vovk <oleg.vovk@mobindustry.net>
2015-05-26 16:38:24 +0300

    Action bar

--- app/src/main/java/net/mobindustry/telegram/ui/activitys/RegistrationActivity.java
+++ app/src/main/java/net/mobindustry/telegram/ui/activitys/RegistrationActivity.java
@@ -1,13 +1,22 @@
 package net.mobindustry.telegram.ui.activitys;
 
 import android.app.Activity;
-import android.app.Fragment;
-import android.app.FragmentTransaction;
+
+
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+
 
 import net.mobindustry.telegram.R;
+import net.mobindustry.telegram.ui.fragments.RegistrationMainFragment;
 
-public class RegistrationActivity extends Activity{
+public class RegistrationActivity extends AppCompatActivity{
 
     private Fragment registrationUserPhone;
     private Fragment chooseCountry;
@@ -19,7 +28,11 @@ public class RegistrationActivity extends Activity{
         setContentView(R.layout.registration_activity);
 
 
+        registrationUserPhone=new RegistrationMainFragment();
 
+        fragmentTransaction=getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.fragmentContainer,registrationUserPhone);
+        fragmentTransaction.commit();
 
 
     }
--- app/src/main/java/net/mobindustry/telegram/ui/fragments/RegistrationMainFragment.java
+++ app/src/main/java/net/mobindustry/telegram/ui/fragments/RegistrationMainFragment.java
@@ -9,11 +9,24 @@ import android.view.ViewGroup;
 
 import net.mobindustry.telegram.R;
 
+/**
+ * Created by alytar on 26.05.15.
+ */
 public class RegistrationMainFragment extends Fragment {
 
- //   @Nullable
-  //  @Override
-   // public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-    //    inflater.inflate();
-   // }
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.registration_main_fragment, container, false);
+
+
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Toolbar toolbar = (Toolbar) getActivity().findViewById(R.id.toolbar);
+        toolbar.setLogo(R.drawable.ic_check);
+    }
 }
