ff53996f33da33a3a76abd27732d2d789518b968
Oleg Vovk <oleg.vovk@mobindustry.net>
2015-05-26 16:42:12 +0300

    Action bar

--- app/src/main/java/net/mobindustry/telegram/ui/fragments/RegistrationMainFragment.java
+++ app/src/main/java/net/mobindustry/telegram/ui/fragments/RegistrationMainFragment.java
@@ -3,6 +3,7 @@ package net.mobindustry.telegram.ui.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
