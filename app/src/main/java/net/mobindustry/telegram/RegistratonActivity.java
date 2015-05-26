759414ef1e8108a967336fa347031b3337970292
Oleg Vovk <oleg.vovk@mobindustry.net>
2015-05-26 12:11:15 +0300

    Add CardView

---
+++ app/src/main/java/net/mobindustry/telegram/RegistratonActivity.java
@@ -0,0 +1,12 @@
+package net.mobindustry.telegram;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RegistratonActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registration_activity);
+    }
+}
