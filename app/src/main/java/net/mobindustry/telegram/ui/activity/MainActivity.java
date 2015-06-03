cf2e8fe6c4c28e0f2abc3fa1cd8ddf8e621df605
alex.tsymbal <alex.tsymbal@mobindustry.net>
2015-06-03 16:34:49 +0300

    main activity rewrite

--- app/src/main/java/net/mobindustry/telegram/ui/activity/MainActivity.java
+++ app/src/main/java/net/mobindustry/telegram/ui/activity/MainActivity.java
@@ -1,9 +1,15 @@
 package net.mobindustry.telegram.ui.activity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v4.app.FragmentManager;
 import android.util.Log;
 
 import com.romainpiel.titanic.library.Titanic;
@@ -11,6 +17,11 @@ import com.romainpiel.titanic.library.TitanicTextView;
 
 import net.mobindustry.telegram.R;
 import net.mobindustry.telegram.messenger.TestConnect;
+import net.mobindustry.telegram.ui.fragments.ContactListFragment;
+
+import org.drinkless.td.libcore.telegram.Client;
+import org.drinkless.td.libcore.telegram.TG;
+import org.drinkless.td.libcore.telegram.TdApi;
 
 import java.util.concurrent.TimeUnit;
 
@@ -18,17 +29,50 @@ public class MainActivity extends Activity {
 
     private SplashStart splashStart;
 
+    private Client client;
+    private Client.ResultHandler resultHandler;
+    private boolean stateWaitCode = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        TitanicTextView tv = (TitanicTextView) findViewById(R.id.titanic_tv);
-        Titanic titanic = new Titanic();
-        titanic.start(tv);
 
-        splashStart = new SplashStart();
-        splashStart.execute();
+        resultHandler = new Client.ResultHandler() {
+            @Override
+            public void onResult(TdApi.TLObject object) {
 
+                Log.i("Log", "Result " + object);
+
+                if (object instanceof TdApi.AuthStateOk) {
+                    stateWaitCode = false;
+                }
+
+                if (object instanceof TdApi.AuthStateWaitSetPhoneNumber) {
+                    stateWaitCode = true;
+                }
+            }
+        };
+
+        TG.setDir(getExternalFilesDir("").getAbsolutePath());
+        TG.setUpdatesHandler(resultHandler);
+
+        client = TG.getClientInstance();
+    }
+
+    public void start() {
+        if (isOnline()) {
+            TitanicTextView tv = (TitanicTextView) findViewById(R.id.titanic_tv);
+            Titanic titanic = new Titanic();
+            titanic.start(tv);
+
+            client.send(new TdApi.AuthGetState(), resultHandler);
+
+            splashStart = new SplashStart();
+            splashStart.execute();
+        } else {
+            checkWiFi();
+        }
     }
 
     public static void showActivityAnimation(Activity activity) {
@@ -43,16 +87,14 @@ public class MainActivity extends Activity {
         }
     }
 
-
     private class SplashStart extends AsyncTask<Void, Void, Void> {
 
         @Override
         protected Void doInBackground(Void... voids) {
-
             try {
-                TimeUnit.SECONDS.sleep(1); //TODO set time 5 second;
+                TimeUnit.SECONDS.sleep(5);
             } catch (InterruptedException e) {
-                Log.i("MainActivityTag", "SplashStart task interrupted");
+                Log.e("Log", "SplashStart task interrupted");
             }
             return null;
         }
@@ -61,10 +103,45 @@ public class MainActivity extends Activity {
         protected void onPostExecute(Void aVoid) {
             super.onPostExecute(aVoid);
 
-            Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
-            startActivity(intent);
-            showActivityAnimation(MainActivity.this);
-            finish();
+            if (stateWaitCode) {
+                Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+                showActivityAnimation(MainActivity.this);
+                finish();
+            } else {
+                Intent intent = new Intent(MainActivity.this, ChatActivity.class);
+                startActivity(intent);
+                showActivityAnimation(MainActivity.this);
+                finish();
+            }
         }
+    }
+
+    public boolean isOnline() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnectedOrConnecting();
+    }
+
+    public void checkWiFi() {
+        if (!this.isOnline()) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Internet is not enable");
+            builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
+                    dialog.cancel();
+                }
+            });
+            AlertDialog alert = builder.create();
+            alert.show();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        start();
     }
 }
