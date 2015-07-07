3f07cb4822876c256c06ae7f3d399005d21324f6
alex.tsymbal <alex.tsymbal@mobindustry.net>
2015-07-07 17:27:28 +0300

    fix loop

--- app/src/main/java/net/mobindustry/telegram/ui/activity/MainActivity.java
+++ app/src/main/java/net/mobindustry/telegram/ui/activity/MainActivity.java
@@ -64,6 +64,7 @@ public class MainActivity extends Activity implements ApiClient.OnApiResultHandl
 
         if (isOnline()) {
             if (DataHolder.isLoggedIn()) {
+                hasAnswer = true;
                 runStartActivity();
             } else {
                 start();
