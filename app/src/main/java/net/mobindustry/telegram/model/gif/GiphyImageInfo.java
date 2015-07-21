98614df24e3002a548db26dd84934a01b3acf98e
Oleg Vovk <oleg.vovk@mobindustry.net>
2015-07-21 17:46:50 +0300

    fix choose file

---
+++ app/src/main/java/net/mobindustry/telegram/model/gif/GiphyImageInfo.java
@@ -0,0 +1,31 @@
+package net.mobindustry.telegram.model.gif;
+
+import java.io.Serializable;
+
+public class GiphyImageInfo implements Serializable{
+
+    private String url;
+    private String width;
+    private String height;
+    private String size;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getWidth() {
+        return width;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public GiphyImageInfo() {
+
+    }
+}
