79216b74f15f47d380db1a8cf168e624e9ab4cf9
Oleg Vovk <oleg.vovk@mobindustry.net>
2015-07-30 10:10:17 +0300

    fix dialog about phone number

--- app/src/main/java/net/mobindustry/telegram/ui/fragments/fragmentDialogs/DialogPhoneNumberInvalid.java
+++ app/src/main/java/net/mobindustry/telegram/ui/fragments/fragmentDialogs/DialogPhoneNumberInvalid.java
@@ -13,7 +13,7 @@ public class DialogPhoneNumberInvalid extends DialogFragment {
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         return new AlertDialog.Builder(getActivity())
                 .setTitle("Error")
-                .setMessage("Invalid phone number")
+                .setMessage("The phone number is invalid")
                 .setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
                         dialog.cancel();
